393963c7aaf4ec75ebf3fee85303e739575d7d6d
XxMaziozarexX <55894764+XxMaziozarexX@users.noreply.github.com>
2022-09-05 14:29:07 -0500

    Barra de navegacion completa.

---
+++ app/src/main/java/mx/itesm/bamx/DineroFragment.kt
@@ -0,0 +1,13 @@
+package mx.itesm.bamx
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class DineroFragment : Fragment(R.layout.fragment_dinero) {
+
+
+}
--- app/src/main/java/mx/itesm/bamx/MainActivity.kt
+++ app/src/main/java/mx/itesm/bamx/MainActivity.kt
@@ -4,20 +4,56 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var botomNav : BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var dineroFragment = DineroFragment()
+        var postulacionesFragment = PostulacionesFragment()
+        var homeFragment = HomeFragment()
+
+        botomNav = findViewById(R.id.bottomNavigationView2)
+
+
+        botomNav.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.nav_coin -> {
+                    serCurrentFragment(dineroFragment)
+                    true
+                }
+
+                R.id.nav_person -> {
+                    serCurrentFragment(postulacionesFragment)
+                    true
+                }
+
+
+                R.id.nav_home -> {
+                    serCurrentFragment(homeFragment)
+                    true
+                }
+                else -> false
+            }
+        }
+
     }
 
-    fun irADM(view : View?) {
-        val intent = Intent(this, donacionesMonetarias::class.java)
-        startActivity(intent)
+
+    private  fun serCurrentFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.containerView, fragment)
+            commit()
+        }
+
+
     }
 
-    fun irADE(view : View?) {
-        val intent = Intent(this, donacionesEspecie::class.java)
-        startActivity(intent)
-    }
 }
