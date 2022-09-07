7f23e8a50a0d5c4d11ea7ddcbc40a32e0ebb0a70
JoseTobon <a01411655@itesm.mx>
2022-09-07 12:08:13 -0500

    navegacion fragments donaciones

--- app/src/main/java/mx/itesm/bamx/DineroFragment.kt
+++ app/src/main/java/mx/itesm/bamx/DineroFragment.kt
@@ -5,9 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import mx.itesm.bamx.databinding.FragmentDineroBinding
+import mx.itesm.bamx.databinding.FragmentHomeBinding
 
 
-class DineroFragment : Fragment(R.layout.fragment_dinero) {
+class DineroFragment : Fragment() {
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
 
+        val bind = FragmentDineroBinding.inflate(layoutInflater)
+        val monetariasFragment = MonetariasFragment()
+        val especieFragment = EspecieFragment()
+
+        bind.button3.setOnClickListener {
+            fragmentManager?.beginTransaction()?.apply {
+                replace(R.id.containerView, monetariasFragment)
+                commit()
+            }
+        }
+
+        bind.button4.setOnClickListener {
+            fragmentManager?.beginTransaction()?.apply {
+                replace(R.id.containerView, especieFragment)
+                commit()
+            }
+        }
+
+        return bind.root
+    }
 }
