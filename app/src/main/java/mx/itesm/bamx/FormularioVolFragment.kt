60cabc91fa4979f3a589b7dd5d2ebdd93ac18ca6
JoseTobon <a01411655@itesm.mx>
2022-09-20 14:07:49 -0500

    formulario voluntarios funcional

--- app/src/main/java/mx/itesm/bamx/FormularioVolFragment.kt
+++ app/src/main/java/mx/itesm/bamx/FormularioVolFragment.kt
@@ -1,19 +1,101 @@
 package mx.itesm.bamx
 
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
-class FormularioVolFragment : Fragment() {
+class FormularioVolFragment : Fragment(R.layout.fragment_formulario_vol) {
+
+    lateinit var nombre : EditText
+    lateinit var empresa : EditText
+    lateinit var correo : EditText
+    lateinit var telefono : EditText
+    lateinit var mensaje : EditText
+
+    lateinit var boton : Button
+
+    var vacio = false
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_formulario_vol, container, false)
+
+        var view = inflater.inflate(R.layout.fragment_formulario_vol, container, false)
+
+        nombre = view.findViewById(R.id.nameVol)
+        empresa = view.findViewById(R.id.empresaVoluntario)
+        correo = view.findViewById(R.id.correoVoluntario)
+        telefono = view.findViewById(R.id.telefonoVoluntario)
+        mensaje = view.findViewById(R.id.mensajeVoluntario)
+
+        boton = view.findViewById(R.id.enviarVoluntario)
+
+        boton.setOnClickListener {
+
+            editTextVacio(nombre, vacio)
+            editTextVacio(empresa, vacio)
+            editTextVacio(correo, vacio)
+            editTextVacio(telefono, vacio)
+            editTextVacio(mensaje, vacio)
+            //editTextVacio(mensaje, vacio)
+
+            Log.wtf("BOTON", "SI FUNCIONO")
+
+            val persona = hashMapOf(
+                "nombre" to nombre.text.toString(),
+                "producto" to empresa.text.toString(),
+                "correo" to correo.text.toString(),
+                "telefono" to telefono.text.toString(),
+                "mensaje" to mensaje.text.toString()
+            )
+
+            if (!vacio) {
+
+                val coleccion : CollectionReference = Firebase.firestore.collection("voluntarios")
+
+                val taskAdd = coleccion.add(persona)
+
+                taskAdd.addOnSuccessListener { documentReference ->
+
+                    Toast.makeText(activity,"id ${documentReference.id}", Toast.LENGTH_SHORT).show()
+                }.addOnFailureListener { error->
+
+                    Toast.makeText(activity,"ERROR AL GUARDADO", Toast.LENGTH_SHORT).show()
+
+                    Log.e("FIRESTORE", "error: $error")
+                }
+
+                nombre.setText("")
+                empresa.setText("")
+                correo.setText("")
+                telefono.setText("")
+                mensaje.setText("")
+            }
+
+        }
+
+        return view
+    }
+
+    fun editTextVacio ( editText: EditText, vacio : Boolean) {
+        if (TextUtils.isEmpty(editText.text)) {
+            editText.setError("Campo Obligatorio")
+            this.vacio = true
+        } else {
+            this.vacio = false
+        }
     }
 
 }
--- app/src/main/java/mx/itesm/bamx/PostulacionesFragment.kt
+++ app/src/main/java/mx/itesm/bamx/PostulacionesFragment.kt
@@ -5,9 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import mx.itesm.bamx.databinding.FragmentPostulacionesBinding
 
 
 class PostulacionesFragment : Fragment(R.layout.fragment_postulaciones) {
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val bind = FragmentPostulacionesBinding.inflate(layoutInflater)
+        val formularioVolFragment = FormularioVolFragment()
 
+        bind.postulacionesBoton.setOnClickListener {
+            fragmentManager?.beginTransaction()?.apply {
+                replace(R.id.containerView, formularioVolFragment)
+                commit()
+            }
+        }
+
+        return bind.root
+    }
 }
