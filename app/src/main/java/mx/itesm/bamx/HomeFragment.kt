ad539a69bce482d34045867e6b963bf2bfc8f701
JoseTobon <a01411655@itesm.mx>
2022-10-13 19:47:34 -0500

    twitter entre otras cosas

--- app/src/main/java/mx/itesm/bamx/HomeFragment.kt
+++ app/src/main/java/mx/itesm/bamx/HomeFragment.kt
@@ -5,9 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
+import androidx.fragment.app.FragmentContainer
 
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
+    lateinit var myWebView: WebView
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        var view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        myWebView = view.findViewById(R.id.webView)
+        myWebView.settings.javaScriptEnabled = true
+
+        var htmlPre = "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"utf-8\"></head><body style='margin:0; pading:0; background-color: black;'>";
+        var htmlCode =
+        " <a class=\"twitter-timeline\" data-lang=\"es\" data-theme=\"dark\" href=\"https://twitter.com/BDAGDL?ref_src=twsrc%5Etfw\">Tweets by BDAGDL</a> <script async src=\"https://platform.twitter.com/widgets.js\" charset=\"utf-8\"></script>"
+        var htmlPost = "</body></html>";
+
+        myWebView.loadDataWithBaseURL("null", htmlPre+htmlCode, "text/html", "UTF-8", null)
+
+        return view
+    }
 }
