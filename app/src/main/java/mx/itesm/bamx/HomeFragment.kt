57090ea39a85f65f00145c7582519a8bb5257894
JoseTobon <a01411655@itesm.mx>
2022-10-18 22:59:08 -0500

    splash

--- app/src/main/java/mx/itesm/bamx/HomeFragment.kt
+++ app/src/main/java/mx/itesm/bamx/HomeFragment.kt
@@ -6,12 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.ImageView
+import androidx.core.view.isVisible
 import androidx.fragment.app.FragmentContainer
 
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
-    lateinit var myWebView: WebView
+    lateinit var myWebView : WebView
+    lateinit var carga : ImageView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,14 +25,24 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         var view = inflater.inflate(R.layout.fragment_home, container, false)
 
         myWebView = view.findViewById(R.id.webView)
+
         myWebView.settings.javaScriptEnabled = true
 
-        var htmlPre = "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"utf-8\"></head><body style='margin:0; pading:0; background-color: black;'>";
+        var htmlPre = "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"utf-8\"></head><body style='margin:0; pading:0; background-color: #243447;'>";
         var htmlCode =
-        " <a class=\"twitter-timeline\" data-lang=\"es\" data-theme=\"dark\" href=\"https://twitter.com/BDAGDL?ref_src=twsrc%5Etfw\">Tweets by BDAGDL</a> <script async src=\"https://platform.twitter.com/widgets.js\" charset=\"utf-8\"></script>"
+        " <a class=\"twitter-timeline\" data-lang=\"es\" data-theme=\"dark\" href=\"https://twitter.com/BDAGDL?ref_src=twsrc%5Etfw\"></a> <script async src=\"https://platform.twitter.com/widgets.js\" charset=\"utf-8\"></script>"
         var htmlPost = "</body></html>";
 
-        myWebView.loadDataWithBaseURL("null", htmlPre+htmlCode, "text/html", "UTF-8", null)
+        myWebView.loadDataWithBaseURL("null", htmlPre+htmlCode+htmlPost, "text/html", "UTF-8", null)
+
+        /*
+        myWebView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                carga.isVisible=false
+                super.onPageFinished(view, url)
+            }
+        }*/
+
 
         return view
     }
